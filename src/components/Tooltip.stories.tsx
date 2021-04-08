5703da09a911b8d5620718dd1140eae9409df686
Kay Wood <kwood15@users.noreply.github.com>
2021-04-08 18:25:24 +0100

    Add storybook stories for Tooltip component

---
+++ src/components/Tooltip.stories.tsx
@@ -0,0 +1,22 @@
+import { Tooltip } from './Tooltip';
+
+export const PositionTop = () => (
+  <Tooltip position="top" content="Tooltip Top Content">
+    Tooltip Top content
+  </Tooltip>
+);
+export const PositionRight = () => (
+  <Tooltip position="right" content="Tooltip Right Content">
+    Tooltip Right Trigger
+  </Tooltip>
+);
+export const PositionBottom = () => (
+  <Tooltip position="bottom" content="Tooltip Bottom Content">
+    Tooltip Bottom Trigger
+  </Tooltip>
+);
+export const PositionLeft = () => (
+  <Tooltip position="left" content="Tooltip Left Content">
+    Tooltip Left Trigger
+  </Tooltip>
+);
