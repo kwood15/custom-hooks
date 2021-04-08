a5bbd7f257eb0733c45a5903e7d9095e21458937
Kay Wood <kwood15@users.noreply.github.com>
2021-04-08 15:28:45 +0100

    Add Tooltip component

---
+++ src/components/Tooltip.tsx
@@ -0,0 +1,48 @@
+import { ReactElement, ReactNode, useState } from 'react';
+
+interface TooltipProps {
+  position: 'top' | 'right' | 'bottom' | 'left';
+  content: string;
+  children: ReactNode;
+}
+
+export function Tooltip({
+  position,
+  content,
+  children
+}: TooltipProps): ReactElement {
+  const [isShown, setIsShown] = useState(false);
+
+  function showTooltip() {
+    setIsShown(true);
+  }
+
+  function hideTooltip() {
+    setIsShown(false);
+  }
+
+  return (
+    <div className="tooltip" onMouseLeave={hideTooltip}>
+      {isShown && (
+        <div
+          id="tooltipDescription"
+          role="tooltip"
+          className={`tooltip__content tooltip__content--${position}`}
+          aria-hidden={!isShown ? 'true' : 'false'}
+        >
+          {content}
+        </div>
+      )}
+      <span
+        tabIndex={0}
+        role="button"
+        aria-describedby="tooltipDescription"
+        className="tooltip__trigger"
+        onClick={showTooltip}
+        onMouseOver={showTooltip}
+      >
+        {children}
+      </span>
+    </div>
+  );
+}
