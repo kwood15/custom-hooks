052a610a6562a4f239d570cba838662e30bb7a8d
Kay Wood <kwood15@users.noreply.github.com>
2021-04-08 02:12:11 +0100

    Setup amplify sign in functionality/Setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
-import { ReactElement } from 'react';
+import { ReactElement, useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
+import Amplify from 'aws-amplify';
+import { AmplifyAuthenticator, AmplifyGreetings } from '@aws-amplify/ui-react';
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import awsExports from './aws-exports';
+
+import Favourites from './components/account/favourites';
+import { SearchResults } from './components/search/components/SearchResults';
+
 // import asyncRouteList from './routes/asyncRouteList';
-import ProtectedApp from './ProtectedApp';
+
 import './assets/scss/main.scss';
 
+Amplify.configure(awsExports);
+
 function App(): ReactElement {
+  const [authState, setAuthState] = useState<AuthState>();
+  const [user, setUser] = useState<any>();
+
+  useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      if (nextAuthState === AuthState.SignedIn) {
+        console.log('signed in!');
+        console.log('user data: ', authData);
+      }
+      if (!authData) {
+        console.log('not signed in');
+      }
+      setAuthState(nextAuthState);
+      setUser(authData);
+    });
+  }, []);
+
   return (
     <Router>
       <nav>
@@ -23,19 +50,37 @@ function App(): ReactElement {
             <Link to="/checkout">Checkout</Link>
           </li>
           <li>
-            <Link to="/login">Sign in/Register</Link>
+            <Link to="/favourites">Favourites</Link>
           </li>
+          {!user && (
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          )}
         </ul>
       </nav>
+
+      {authState === AuthState.SignedIn && user ? (
+        <>
+          <AmplifyGreetings username={user.username}></AmplifyGreetings>
+        </>
+      ) : (
+        <Route path="/login">
+          <AmplifyAuthenticator />
+        </Route>
+      )}
+
       <Switch>
         <Route path="/" exact>
           homepage
         </Route>
-        <Route path="/search-results">results</Route>
+        <Route path="/search-results">
+          <SearchResults />
+        </Route>
         <Route path="/basket">basket</Route>
         <Route path="/checkout">checkout</Route>
-        <Route path="/login">
-          <ProtectedApp />
+        <Route path="/favourites">
+          <Favourites />
         </Route>
       </Switch>
     </Router>
