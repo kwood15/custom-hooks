5247a9f6b754cb941bf61af376849cbb1a7c295f
Kay Wood <kwood15@users.noreply.github.com>
2021-04-09 19:03:31 +0100

    Add Dropdown component

---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,49 @@
+import { ReactElement, ReactNode, ChangeEvent, FocusEvent } from 'react';
+
+interface DropdownProps {
+  id: string;
+  label?: string;
+  name?: string;
+  required?: boolean;
+  error?: boolean;
+  onChange?: (e: ChangeEvent<HTMLSelectElement>) => void;
+  onBlur?: (e: FocusEvent<HTMLSelectElement>) => void;
+  autofocus?: boolean;
+  className?: string;
+  labelClass?: string;
+  fieldClass?: string;
+  value?: string | number;
+  children: ReactNode;
+}
+
+export default function Dropdown({
+  id,
+  label,
+  name,
+  required,
+  error,
+  onChange,
+  onBlur,
+  autofocus,
+  className,
+  labelClass,
+  fieldClass,
+  value,
+  children,
+  ...props
+}: DropdownProps): ReactElement {
+  return (
+    <div className={`dropdown ${className}`}>
+      <label className={`dropdown__label ${labelClass}`}>{label}</label>
+      <select
+        {...props}
+        id={id}
+        className={`dropdown__field ${fieldClass}`}
+        value={value}
+        onChange={onChange}
+      >
+        {children}
+      </select>
+    </div>
+  );
+}
