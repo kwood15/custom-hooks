a9b652d5aea9beea1ff5902e757bafc6b08f9fe8
Kay Wood <kwood15@users.noreply.github.com>
2021-04-13 11:15:58 +0100

    Setup graphql mutation for add to shopping list

--- src/components/account/shopping-list/index.tsx
+++ src/components/account/shopping-list/index.tsx
@@ -1,6 +1,9 @@
 import { ReactElement, useState, useEffect } from 'react';
 import { API, graphqlOperation } from 'aws-amplify';
 import { listTodos } from '../../../graphql/queries';
+import { createTodo } from '../../../graphql/mutations';
+import { useForm } from '../../../custom-hooks/useForm';
+import { TextField } from '../../TextField';
 
 interface ShoppingListState {
   id: string | null;
@@ -8,11 +11,15 @@ interface ShoppingListState {
   description: string;
 }
 
-// const initialState = { id: '', name: '', description: '' };
+const initialState = { id: '', name: '', description: '' };
 
 function ShoppingList(): ReactElement {
-  // const [formData, setFormData] = useState<ShoppingListState>(initialState);
-  const [items, setItems] = useState([]);
+  const [values, handleChange] = useForm<any>(initialState);
+  const [items, setItems] = useState<ShoppingListState[]>([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   async function fetchData() {
     try {
@@ -25,12 +32,37 @@ function ShoppingList(): ReactElement {
     }
   }
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  async function addItems() {
+    try {
+      if (!values.name || !values.description) return;
+      setItems(values);
+      await API.graphql(
+        graphqlOperation(createTodo, {
+          input: values
+        })
+      );
+    } catch (error) {
+      console.log('There has been a problem adding your item');
+    }
+  }
 
   return (
     <>
+      <h3>Add Item</h3>
+      <TextField
+        id="name"
+        name="name"
+        onChange={handleChange}
+        value={values.name}
+      />
+      <TextField
+        id="description"
+        name="description"
+        onChange={handleChange}
+        value={values.description}
+      />
+      <button onClick={addItems}>Add item</button>
+      <h3>Shopping List</h3>
       {items.map((item: ShoppingListState) => (
         <div key={item.id}>
           <p>{item.name}</p>
